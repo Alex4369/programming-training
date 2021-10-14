6ac28bc66f3df8ec56f10d38eb497b9d88620f03
Aleksei Mamontov <amamont868617@gmail.com>
2021-10-14 09:13:08 +0300

    studies

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        int currentBalance = 2000000000;
-        int transferAmount = 500000000;
+        int currentBalance = 2000_000_000;
+        int transferAmount = 500_000_000;
         int totalAmount = currentBalance + transferAmount;
         System.out.println(totalAmount);
     }
